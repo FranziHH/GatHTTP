e28e60c9d6821ef2ff6bc77ad35d61d18e706254
FranziHH <astrid.gruenklee@yahoo.de>
2024-11-07 16:19:02 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# setting up python virtual environment
+After clone or pull from Github 
+Execute:
+virtualenv .env && source .env/bin/activate && pip install -r requirements.txt
+
+# GatHTTP
+- Request from the server whether access is granted
---
+++ test.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+# https://www.lambdatest.com/blog/python-configparser-tutorial/
+import configparser
+import requests
+import xml.dom.minidom
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+def getXmlValue(node):
+    try:
+        return dom.getElementsByTagName(node)[0].firstChild.nodeValue
+    except Exception as e:
+        # return e.args[0]
+        return "ERROR"
+
+
+# print(config.sections())
+req_url = config['request']['url']
+
+print ('Config Vars:')
+print(req_url)
+
+r = requests.get(req_url)
+# print('Status Code:')
+# print(r.status_code)
+
+print(r.text)
+dom = xml.dom.minidom.parseString(r.text)
+
+# element = dom.getElementsByTagName('displayText')[0]
+print('access:', getXmlValue('access'))
+print('direction:', getXmlValue('direction'))
+print('displayText:', getXmlValue('displayText'))
+# print('ticketId:', getXmlValue('ticketId'))
