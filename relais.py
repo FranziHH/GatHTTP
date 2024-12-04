6a79b61a0d12b0f65590c4535c46657caf5d1601
Franziska Walter <f.walter@portalum.eu>
2024-12-04 13:19:13 +0100

    update functions for RS485

--- relais.py
+++ relais.py
@@ -7,10 +7,16 @@ from func_relais import *
 import time
 
 # RelaisOn(1, 4)
+# print(ByteArr2Hex(RelaisOn(1, 0)))
+
 # setStatus(1, '01010101')
-RelaisOff(1, 255)
+print(ByteArr2Hex(RelaisOff(1, 255)))
 time.sleep(0.5)
 
 
 print(getStatus(1, 255))
 # print(hex(getStatus(1, 4)))
+
+# print(ByteArr2Hex(ReadAddr()))
+
+# print(ReadVersion())
