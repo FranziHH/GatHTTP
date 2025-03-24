b0a9b88d1703ff226d0fc8a9dec53d48802c394b
Franziska Walter <f.walter@portalum.eu>
2025-03-24 17:37:02 +0100

    update CodeReader class

--- test_code_reader.py
+++ test_code_reader.py
@@ -4,6 +4,6 @@ from classes.ServiceCodeReader import *
 import time
 
 reader = ServiceCodeReader()
-barcode_data = reader.decode_barcode("https://mcdonalds.fast-insight.com/voc/at/de?QRCODE=true&CODE=CC9F-N4GY-K7CC-6CCN-CHTR")
+barcode_data = reader.decode_barcode("de?QRCODE=true&CODE=CC9F-N4GY-K7CC-6CCN-CHTR")
 # barcode_data = reader.decode_barcode("CC9F-N4GY-K7CC-6CCN-CHTR")
 print(barcode_data)
